48ec798fa09f543ddbbfd26023865eeeb0eb42a0
Alexander Stach <stach.alex@yahoo.de>
2025-11-08 12:34:35 +0100

    API Setup
    Die API Einstellungen wurden vorgenommen und wichtige NuGet Packages installiert

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,94 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        //  Sammelt alle Produkte aus der Datenbank
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        //  Sammelt ein Produkt anhand der ID aus der Datenbank
+        [HttpGet("{id:int}")] 
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        //  Erstellt ein neues Produkt in der Datenbank
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            context.Products.Add(product);
+            await context.SaveChangesAsync();
+            return product;
+        }
+
+        //  Aktualisiert ein bestehendes Produkt in der Datenbank
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest("Cannot update this product");
+            }
+            context.Entry(product).State = EntityState.Modified;
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        //  Prüft, ob ein Produkt mit der angegebenen ID existiert
+        private bool ProductExists(int id)
+        {
+            return context.Products.Any(e => e.Id == id);
+        }
+
+        //  Löscht ein Produkt aus der Datenbank
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+    }
+}
+
+
